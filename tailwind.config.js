b1d4a407acc5a40474540d7ad85bcf1cfcb38c89
sagar mallick <sagarmallick@quokkalabs.com>
2024-05-07 17:56:45 +0530

    (feat): setup nextjs project with tailwind and framer motion, husky, prettier and Eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        black: '#313131',
+        blue: '#1B6FE9',
+        'dark-blue': '#1453C8',
+        'white-chalk': '#E6EAF1',
+        'blue-chalk': '#BAD3F8',
+        'neon-blue': '#70FBF3',
+      },
+      fontFamily: {
+        IBM: ['IBM Plex Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
